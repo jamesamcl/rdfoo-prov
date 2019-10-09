dfd1c93d71ce1a759100b5f8feca27bc359ae2a1
James Alastair McLaughlin <james@mclgh.net>
2019-10-09 22:30:33 +0100

    Add Domain

---
+++ index.ts
@@ -0,0 +1,16 @@
+
+import Activity from './prov/Activity'
+import Agent from './prov/Agent'
+import Association from './prov/Association'
+import Plan from './prov/Plan'
+import Prov from './prov/Prov'
+import Usage from './prov/Usage'
+
+export {
+    Activity,
+    Agent,
+    Association,
+    Plan,
+    Prov,
+    Usage
+}
