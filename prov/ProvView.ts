18de616e8a50b442d50b1777162efd2fa6049947
James McLaughlin <james@mclgh.net>
2020-05-10 21:29:19 +0100

    add more provview functions

--- prov/ProvView.ts
+++ prov/ProvView.ts
@@ -33,6 +33,26 @@ export default class ProvView extends GraphViewBasic {
 
     }
 
+    get activities():Activity[] {
+        return this.instancesOfType(Types.Prov.Activity).map(uri => new Activity(this, uri))
+    }
+
+    get agents():Agent[] {
+        return this.instancesOfType(Types.Prov.Agent).map(uri => new Agent(this, uri))
+    }
+
+    get associations():Association[] {
+        return this.instancesOfType(Types.Prov.Association).map(uri => new Association(this, uri))
+    }
+
+    get plans():Plan[] {
+        return this.instancesOfType(Types.Prov.Plan).map(uri => new Plan(this, uri))
+    }
+
+    get usages():Usage[] {
+        return this.instancesOfType(Types.Prov.Usage).map(uri => new Usage(this, uri))
+    }
+
 }
 
 
